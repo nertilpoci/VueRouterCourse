d0e1951cd1626fd7d3aed0a7b5e881c2e4a7740a
Nertil Poci <nertilpoci@hotmail.com>
2021-02-26 10:07:54 +0100

    default

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -1,7 +1,3 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-Vue.use(VueMaterial)
 createApp(App).mount('#app')
