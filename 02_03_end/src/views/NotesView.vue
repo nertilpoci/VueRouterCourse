e51d3348c318d942aa931c30834c8d60e0792632
Nertil Poci <nertilpoci@hotmail.com>
2021-04-05 07:19:12 +0200

    02_03 start

---
+++ 02_03_end/src/views/NotesView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row g-0">
+    <div class="col-2">
+      <Labels />
+    </div>
+    <div class="col-10">
+      <Notes />
+    </div>
+  </div>
+</template>
+<script>
+import Notes from "../components/notes/Notes.vue";
+import Labels from "../components/labels/LabelList";
+export default {
+  components: {
+    Notes,
+    Labels,
+  },
+};
+</script>
