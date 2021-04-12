38684f942972d3bc5a1ba022bf9edf805e2a04b3
Nertil Poci <nertilpoci@hotmail.com>
2021-04-12 04:05:53 +0200

    update

---
+++ 04_02_start/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container-fluid p-0">
+  <router-view  name="Navbar"></router-view>
+  
+<div class='container-fluid'>
+  <router-view></router-view>
+
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+.card{
+   break-inside: avoid!important;
+}
+.card-columns{
+  column-count: 4;
+}
+</style>
